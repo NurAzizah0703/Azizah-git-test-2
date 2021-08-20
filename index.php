6ffdd05f18df833938360bdaa039b45735dda2c3
NurAzizah07 <nurazizah12594@gmail.com>
2021-08-20 08:35:50 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello Word!</h1>    
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nobis rem ut officia, deserunt error quasi sit porro nostrum voluptate voluptas possimus obcaecati suscipit facere, quis odio mollitia expedita. Tempore?</p>
+    <ul>
+    
+    <li> satu </li>
+    <li>dua</li>
+    </ul>
 </body>
 </html>
