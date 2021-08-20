fc523bcf838b3cc133a73e81c9e83947d3c55f5a
NurAzizah07 <nurazizah12594@gmail.com>
2021-08-20 08:23:37 +0700

    menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git remote</title>
+</head>
+<body>
+    <h1>Hello Word!</h1>    
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id nobis rem ut officia, deserunt error quasi sit porro nostrum voluptate voluptas possimus obcaecati suscipit facere, quis odio mollitia expedita. Tempore?</p>
+</body>
+</html>
